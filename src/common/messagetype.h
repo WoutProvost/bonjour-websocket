306cbf5e0c5c3f4b286a21ffcd043f2d40caef05
WoutProvost <wout_provost@hotmail.com>
2021-01-23 08:31:28 +0100

    Add messagetype notes

--- src/common/messagetype.h
+++ src/common/messagetype.h
@@ -3,11 +3,11 @@
 
 enum MessageType
 {
-	NONE,
+	NONE,	// Used when the 'type' field is absent
 	ALL,
 	ADD_OR_UPDATE,
 	REMOVE,
-	REFRESH
+	REFRESH	// Client can manually ask for a refresh
 };
 
 #endif
